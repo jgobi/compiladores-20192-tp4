a2db6d38f08895780e142b99d353b4e604a74bda
João Gabriel de Oliveira Bicalho <jgobi@ufmg.br>
2019-11-25 09:49:59 -0300

    tabela de símbolos

---
+++ symbol_table.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "symbol_table.h"
+
+symbol_table_t* st_alloc (unsigned max_size) {
+    symbol_table_t* st = (symbol_table_t*) malloc(sizeof (symbol_table_t));
+    st->max = max_size;
+    st->len = 0;
+    st->cur_block = 0;
+    st->nodes = (st_node_t*) malloc(max_size * sizeof (st_node_t));
+    return st;
+};
+void st_free (symbol_table_t *st) {
+    free(st->nodes);
+    free(st);
+};
+st_node_t* st_lookup (symbol_table_t *st, char *symbol) {
+    for (unsigned i = 0; i<(st->len); i++) {
+        if (strcmp((st->nodes + i)->name, symbol) == 0) {
+            return st->nodes + i;
+        }
+    }
+    return NULL;
+};
+
+st_node_t* st_insert (symbol_table_t *st, char *symbol, st_type_t type, unsigned line) {
+    st_node_t* node = st->nodes + st->len;
+    st->len++;
+    node->block = st->cur_block;
+    node->line = line;
+    node->name[0] = '\0';
+    node->type = type;
+    strcpy(node->name, symbol);
+    return node;
+};
+
+void st_print (symbol_table_t *st) {
+    printf("idx\tblock\tline\ttype\t\tidentifier\n");
+    for (unsigned i = 0; i<(st->len); i++) {
+        st_node_t* node = (st->nodes + i);
+        printf("%u\t%u\t%u\t%s\t\t%s\n", i, node->block, node->line, st_types[node->type], node->name);
+    }
+    printf("\nlength: %u\tmax size: %u\tfree: %u\n", st->len, st->max, st->max-st->len);
+};
+
+unsigned st_enter_block (symbol_table_t *st) {
+    return ++(st->cur_block);
+};
+
+unsigned st_leave_block (symbol_table_t *st) {
+    return --(st->cur_block);
+};
---
+++ symbol_table.h
@@ -0,0 +1,29 @@
+#ifndef SYMBOL_TABLE__H
+#define SYMBOL_TABLE__H
+
+typedef enum st_type_t { BOOLEAN_T, CHAR_T, INTEGER_T, REAL_T } st_type_t;
+char st_types[4][8] = { "boolean", "char", "integer", "real" };
+
+typedef struct st_node_t {
+    char name[64];
+    st_type_t type;
+    int block;
+    int line;
+} st_node_t;
+
+typedef struct symbol_table_t {
+    unsigned len;
+    unsigned max;
+    unsigned cur_block;
+    st_node_t* nodes;
+} symbol_table_t;
+
+symbol_table_t* st_alloc (unsigned max_size);
+void st_free (symbol_table_t *st);
+st_node_t* st_lookup (symbol_table_t *st, char *symbol);
+st_node_t* st_insert (symbol_table_t *st, char *symbol, st_type_t type, unsigned line);
+
+unsigned st_enter_block (symbol_table_t *st);
+unsigned st_leave_block (symbol_table_t *st);
+
+#endif  // SYMBOL_TABLE__H
